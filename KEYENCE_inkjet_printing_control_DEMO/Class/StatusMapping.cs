1f4573915b504d3c81b4ee8ce3097782317a9982
TheerasakRung <rungworking@gmail.com>
2025-10-07 10:24:23 +0700

    ีupdate path status mapping

--- KEYENCE_inkjet_printing_control_DEMO/Class/StatusMapping.cs
+++ KEYENCE_inkjet_printing_control_DEMO/Class/StatusMapping.cs
@@ -41,12 +41,17 @@ namespace StatusMapping
         private StatusData _statusData;
         private ErrorCollection _errorCollection;
 
-        private string jsonPathStatus = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "status_mapping.json");
+        //private string jsonPathStatus = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "status_mapping.json");
         private string jsonPathError = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "communication_errors.json");
 
         // โหลดไฟล์ status_mapping.json
         public bool LoadStatus()
         {
+            string projectRoot = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)
+                              .Parent?.Parent?.Parent?.FullName ?? "";
+
+            string jsonPathStatus = Path.Combine(projectRoot, "KEYENCE_inkjet_printing_control_DEMO", "Data", "status_mapping.json");
+
             if (!File.Exists(jsonPathStatus))
                 return false;
 
